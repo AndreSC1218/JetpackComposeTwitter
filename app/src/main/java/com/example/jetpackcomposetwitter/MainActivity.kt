7bc3aacc3e87a61e48a221336fb688ba9d14d369
André Salazar <71437593+AndreSC1218@users.noreply.github.com>
2023-06-23 16:03:20 +0200

    ADD: TwitterCard, TweetDivider
    ADD: TwitterCard, TweetDivider

--- app/src/main/java/com/example/jetpackcomposetwitter/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetwitter/MainActivity.kt
@@ -5,18 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetwitter.ui.theme.JetpackComposeTwitterTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,10 +29,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeTwitterTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Column(
+                    modifier = Modifier.fillMaxSize().background(Color(0XFF161D26))
                 ) {
+                    TwitterCard()
+                    TweetDivider()
                     TwitterCard()
                 }
             }
@@ -39,6 +44,10 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun TwitterCard() {
+    var chat by remember { mutableStateOf(false) }
+    var rt by remember { mutableStateOf(false) }
+    var like by remember { mutableStateOf(false) }
+
     Row(
         Modifier
             .fillMaxWidth()
@@ -52,8 +61,154 @@ fun TwitterCard() {
                 .clip(CircleShape)
                 .size(55.dp)
         )
-        Column(Modifier.fillMaxWidth().padding(16.dp)) {
-            
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Row(Modifier.fillMaxWidth()) {
+                TextTitle("André", Modifier.padding(end = 8.dp))
+                DefaultText("@AndreSC", Modifier.padding(end = 8.dp))
+                DefaultText("5h", Modifier.padding(end = 8.dp))
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(
+                    painterResource(id = R.drawable.ic_dots),
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
+            TextBody(
+                "¡Hola mundo! Esto es una practica de Jetpack Compose",
+                Modifier.padding(bottom = 16.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(10)), contentScale = ContentScale.Crop
+            )
+            Row(Modifier.padding(top = 16.dp)) {
+
+                //COMENTARIOS
+                SocialIcon(
+                    modifier = Modifier.weight(1f),
+                    unselectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_chat),
+                            "",
+                            tint = Color(0XFF7E8898)
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_chat_filled),
+                            "",
+                            tint = Color(0XFF7E8898)
+                        )
+                    },
+                    isSelected = chat
+                ) {
+                    chat = !chat
+                }
+                //RT
+                SocialIcon(
+                    modifier = Modifier.weight(1f),
+                    unselectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_rt),
+                            "",
+                            tint = Color(0XFF7E8898)
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_rt),
+                            "",
+                            tint = Color(0XFF7E8898)
+                        )
+                    },
+                    isSelected = rt
+                ) {
+                    rt = !rt
+                }
+                //like
+                SocialIcon(
+                    modifier = Modifier.weight(1f),
+                    unselectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_like),
+                            "",
+                            tint = Color(0XFF7E8898)
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_like_filled),
+                            "",
+                            tint = Color(0XFF7E8898)
+                        )
+                    },
+                    isSelected = like
+                ) {
+                    like = !like
+                }
+
+            }
         }
     }
+}
+
+@Composable
+fun SocialIcon(
+    modifier: Modifier,
+    unselectedIcon: @Composable () -> Unit,
+    selectedIcon: @Composable () -> Unit,
+    isSelected: Boolean,
+    onItemSelected: () -> Unit
+) {
+    val defaultValue = 1
+
+    Row(
+        modifier = modifier.clickable { onItemSelected },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (isSelected) {
+            selectedIcon()
+        } else {
+            unselectedIcon()
+        }
+
+        Text(
+            text = if (isSelected) (defaultValue + 1).toString() else defaultValue.toString(),
+            color = Color(0XFF7E8898),
+            fontSize = 12.sp,
+            modifier = Modifier.padding(start = 4.dp)
+        )
+    }
+}
+
+@Composable
+fun TextBody(text: String, modifier: Modifier = Modifier) {
+    Text(text = text, color = Color.White, modifier = modifier)
+}
+
+@Composable
+fun TextTitle(title: String, modifier: Modifier = Modifier) {
+    Text(text = title, color = Color.White, fontWeight = FontWeight.ExtraBold, modifier = modifier)
+}
+
+@Composable
+fun DefaultText(title: String, modifier: Modifier = Modifier) {
+    Text(text = title, color = Color.Gray, modifier = modifier)
+}
+
+@Composable
+fun TweetDivider(){
+    Divider(
+        Modifier
+            .padding(4.dp)
+            .height(0.5.dp)
+            .fillMaxWidth(),color = Color(0XFF7E8898))
 }
